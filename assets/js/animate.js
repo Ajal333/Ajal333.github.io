f152572f292d7cce2869290ff4d2be5bc45bcec2
Ajal <ajapoos1999@gmail.com>
2020-10-06 23:05:28 +0530

    updates

--- assets/js/animate.js
+++ assets/js/animate.js
@@ -10,14 +10,14 @@ function heroHeadingAnimation() {
   });
 }
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    const loader = document.querySelector("#load");
-    loader.classList.add("endLoad");
-    const name = document.querySelector("#name");
-    name.classList.add("showName");
-  }, 2000);
-});
+// window.addEventListener("load", () => {
+//   setTimeout(() => {
+//     const loader = document.querySelector("#load");
+//     loader.classList.add("endLoad");
+//     const name = document.querySelector("#name");
+//     name.classList.add("showName");
+//   }, 2000);
+// });
 
 // window.addEventListener("mousemove", (e) => {
 //   let mouseCursor = document.querySelector(".cursor");
